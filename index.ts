b2da134fb887c7dde190c06289e9743ca5b4837f
doda-m <dodare.masahiro@gmail.com>
2021-06-01 16:54:41 +0900

    about let

--- index.ts
+++ index.ts
@@ -1,47 +1,65 @@
-var title: string = 'Angular';
-title = 'Angular!';
+// var title: string = 'Angular';
+// title = 'Angular!';
 
-var n: number = 0;
+// var n: number = 0;
 
-var isOpen: boolean = true;
+// var isOpen: boolean = true;
 
-var keyword: (string | number)[] = ['name', 'email', 'body', 0];
+// var keyword: (string | number)[] = ['name', 'email', 'body', 0];
 
-var payment: [number, string, number] = [1, 'apple', 0];
+// var payment: [number, string, number] = [1, 'apple', 0];
 
-var object: { [key: string]: string } = {
-  name: 'Masahiro Dodare',
-  id: 'test'
-};
+// var object: { [key: string]: string } = {
+//   name: 'Masahiro Dodare',
+//   id: 'test'
+// };
 
-var post: object = [
-  {
-    id: 1,
-    content: 'lorem ipsum'
-  }
-];
+// var post: object = [
+//   {
+//     id: 1,
+//     content: 'lorem ipsum'
+//   }
+// ];
 
-var sample: string | number;
-sample = 'string';
-sample = 100;
+// var sample: string | number;
+// sample = 'string';
+// sample = 100;
 
-enum KeyCode {
-  Backspace = 8,
-  Enter = 13
+// enum KeyCode {
+//   Backspace = 8,
+//   Enter = 13
+// }
+
+// console.log(KeyCode.Backspace);
+
+// enum ErrorCode {
+//   OK = 200,
+//   BAD_RESPONSE = 400,
+//   UNAUTHORIZED = 401
+// }
+
+// enum Character {
+//   Bob,
+//   Dad,
+//   Mom
+// }
+
+// console.log(Character.Bob);
+
+// var something: any = 'foo';
+// something = 100;
+// something = true;
+// something = { id: 1 };
+
+// var el: Element | null = document.querySelector('#app');
+// var foo: undefined = undefined;
+
+// var unknown = 'string';
+
+// let foo = 'foo';
+let data = new Date();
+
+if (true) {
+  let foo = 'foo';
 }
-
-console.log(KeyCode.Backspace);
-
-enum ErrorCode {
-  OK = 200,
-  BAD_RESPONSE = 400,
-  UNAUTHORIZED = 401
-}
-
-enum Character {
-  Bob,
-  Dad,
-  Mom
-}
-
-console.log(Character.Bob);
+// console.log(foo);
